94c16074f1ba022a2063c316ad6f331cd5b57cbe
Chris Bracken <chris@bracken.jp>
2015-11-04 16:40:06 -0800

    Add test/all_tests.dart for coverage.

--- tool/travis.sh
+++ tool/travis.sh
@@ -10,8 +10,7 @@ set -e
 # Verify that the libraries are error and warning-free.
 echo "Running dartanalyzer..."
 libs=$(find lib -maxdepth 1 -type f -name '*.dart')
-testing_libs=$(find lib/testing -maxdepth 1 -type f -name '*.dart')
-dartanalyzer $DARTANALYZER_FLAGS $libs $testing_libs test/all_tests.dart
+dartanalyzer $DARTANALYZER_FLAGS $libs test/all_tests.dart
 
 # Run the tests.
 echo "Running tests..."
